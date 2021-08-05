80857c8e381b0dfa20ba9d8a8790efd67d02d911
piguide.dev <nhatco@gmail.com>
2021-08-05 19:39:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
 sudo -E apt-get -qy update >> /tmp/install.log 2>&1
 
-echo "chuyen qua root"
-sudo su >> /tmp/install.log 2>&1
 
 echo "Tai code tu github ve"
 sudo git clone https://github.com/tminhnhat/TradingView-Webhook-Bot.git >> /tmp/install.log 2>&1

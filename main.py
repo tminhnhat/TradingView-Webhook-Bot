5b3fb3be7e3813d0b28503f1066d3b93756e86e6
piguide.dev <nhatco@gmail.com>
2021-08-06 20:30:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,32 @@ def webhook():
         print("[X]", get_timestamp(), "Error:\n>", e)
         return "Error", 400
 
+@app.route("/webhook1", methods=["POST"])
+def webhook1():
+    try:
+        if request.method == "POST":
+            data1 = request.get_json()
+            key1 = data["key"]
+            sleeptime2 = data["sleeptime"]
+            sleeptime3 = int(sleeptime2)
+            if key1 == config.sec_key1:
+                print(get_timestamp(), "Alert Received & Sent!")
+                send_alert(data)
+                return "Sent alert", 200
+            elif key1 == config.sec_key2:
+                time.sleep(sleeptime3)
+                print(get_timestamp(), "Alert Received & Sent!")
+                send_alert(data)
+                return "Sent alert", 200
+            else:
+                
+                print("[X]", get_timestamp(), "Alert Received & Refused! (Wrong Key)")
+                return "Refused alert", 400
 
+    except Exception as e:
+        print("[X]", get_timestamp(), "Error:\n>", e)
+        return "Error", 400
+        
 if __name__ == "__main__":
     from waitress import serve
 
